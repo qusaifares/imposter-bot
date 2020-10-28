cf6edca49a8d473e43adc663626f307b08ecdc6e
Qusai Fares <qusaifares@gmail.com>
2020-10-28 01:47:31 -0400

    feat: add types

---
+++ src/libs/amongUs.ts
@@ -0,0 +1,3 @@
+export type Color = 'red' | 'blue' | 'green' | 'pink' | 'orange' | 'yellow' | 'black' | 'white' | 'purple' | 'brown' | 'cyan' | 'lime'
+
+export type Region = 'NA' | 'EU' | 'ASIA' | string
